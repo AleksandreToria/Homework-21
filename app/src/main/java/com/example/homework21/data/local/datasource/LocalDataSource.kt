fc6d967ff8fefe90bf9f8e30db9bad51986b85c7
Aleksandre Toria <aleksandretoria1@gmail.com>
2024-02-02 00:04:54 +0400

    Changed database implementation functionality to run on the Dispatchers.IO to prevent main thread blockage.

--- app/src/main/java/com/example/homework21/data/local/datasource/LocalDataSource.kt
+++ app/src/main/java/com/example/homework21/data/local/datasource/LocalDataSource.kt
@@ -5,22 +5,23 @@ import com.example.homework21.data.local.mapper.toData
 import com.example.homework21.data.local.mapper.toDomain
 import com.example.homework21.domain.local.LocalRepository
 import com.example.homework21.domain.model.GetItems
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class LocalDataSource @Inject constructor(
     private val itemDao: ItemDao
 ) : LocalRepository {
-    override suspend fun getItems(): Flow<List<GetItems>> {
-        return itemDao.getAllItems().map { entities ->
-            entities.map {
-                it.toDomain()
-            }
+    override suspend fun getItems(): Flow<List<GetItems>> = withContext(Dispatchers.IO) {
+        itemDao.getAllItems().map { entities ->
+            entities.map { it.toDomain() }
         }
     }
 
-    override suspend fun saveItems(items: List<GetItems>) {
+    override suspend fun saveItems(items: List<GetItems>) = withContext(Dispatchers.IO) {
         items.forEach {
             itemDao.insertItem(it.toData())
         }
